cc522443853fdf4c9486d63e7a8247c77da0cc70
Kristine Fernandez <hello@kristinefernandez.com>
2023-07-04 11:04:05 -0500

    spacing

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import { FontLoader } from 'three/examples/jsm/loaders/FontLoader.js';
 //import font from 'three/examples/fonts/helvetiker_regular.typeface.json'
 
 import { Canvas, useFrame, useLoader, extend } from '@react-three/fiber'
-import { OrbitControls } from '@react-three/drei'
+import { OrbitControls, SoftShadows } from '@react-three/drei'
 import * as THREE from 'three';
 import myFont from './flash_rogers.typeface.json';
 
@@ -19,7 +19,7 @@ const ModelViewer = ({ model, position, rotation, scale, clickHandler }) => {
     const gltf = useLoader(GLTFLoader, model)
     // apply position to model
 
-    return <primitive   onClick={ clickHandler } object={gltf.scene} position={position} rotation={rotation} scale={ scale } />
+    return <primitive  castShadow onClick={ clickHandler } object={gltf.scene} position={position} rotation={rotation} scale={ scale } />
  }
 
 const Logo = () => {
@@ -47,91 +47,11 @@ const Logo = () => {
     </>) 
 };
 
-const Car = ({ position, rotation, body, setBodyIndex }) => {
-
-    return (
-        <group rotation={ rotation } position={ position }>
-            <Wheels />
-            <Body body={ body} setBodyIndex={ setBodyIndex} />
-        </group>
-    )
-};
-
-const Axel = () => {
-    extend({CylinderGeometry})
-    return (<>
-        <mesh position={[0,1,-2]} rotation={ [0,0,Math.PI/2] }>
-            
-            <cylinderGeometry attach="geometry" args={[0.05,0.05,4]} />
-            <meshPhongMaterial attach="material" color="#ffffff" />
-        </mesh>
-    </>)
-}
-        
-
-const Wheels = () => {
-    return (<>
-        <BackWheels />
-        <FrontWheels position={[0,0,1] } scale={[0.8,0.8,0.8]}/>
-    </>)
-};
-
-const BackWheels = ({ position }) => {
-    return (<>
-        <group position={ position }>
-            <Axel />
-            <ModelViewer
-                model="./assets/models/wheels.gltf"                
-            />
-        </group>
-    </>)
-};
-
-
-const FrontWheels = ({ position, scale }) => {
-    return (<>
-        <group position={ position } scale={ scale }>
-            <Axel/>
-            <ModelViewer
-                model="./assets/models/wheels-front.gltf"                                
-            />
-        </group>
-    </>)
-};
-
-let bodies = ['jordan','heart','kitten','nugget','torso'];    
-
-const changeBody = (currBody) => {
-    let bodyIndex = currBody;
-    bodyIndex+= 1;
-    if (bodyIndex > bodies.length - 1) {
-        bodyIndex = 0;
-    }
-    return bodyIndex; 
-}
-
-// on click change body
-
-
-const Body = ({position, body, setBodyIndex, rotation, scale}) => {    
-
-    const bodyAsset = bodies[body];
-
-    return (<>
-        <ModelViewer
-            clickHandler = { () => setBodyIndex(changeBody(body)) }
-            model={ `/assets/models/body/${ bodyAsset }.gltf` }
-            position={position}
-            rotation={ rotation }
-            scale={ scale }
-        />
-    </>)
-};
 
 const Ground = () => {
     extend({PlaneGeometry})
     return (<>
-        <mesh position={[0,0,-12]}>
+        <mesh position={[0,0,-16]}>
             
             <planeGeometry attach="geometry" args={[100,100]} />
             <meshPhongMaterial attach="material" color="#000066" />
@@ -142,19 +62,107 @@ const Ground = () => {
 const Base = () => {
     extend({CylinderGeometry})
     return (<>
-        <mesh position={[0,-4,-9]} rotation={ [0,0,0] }  >
+        <mesh receiveShadow position={[0,-4,-9.85]} rotation={ [0,0,0] }  >
             
-            <cylinderGeometry attach="geometry" args={[5,5,1]} />
-            <meshPhysicalMaterial attach="material" color="#333" />
+            <shadowMaterial transparent opacity={0.4} />
+            <cylinderGeometry attach="geometry" args={[5,5,0.4,50]} />
+            <meshLambertMaterial color="#666" roughness={0} metalness={1} />
         </mesh>
     </>)
 };
 
+const Configurator = () => {
+    let bodies = ['jordan','heart','kitten','nugget','torso'];
+    const [body, setBody] = useState('jordan');
+        
+    const Car = ({ position, rotation, body, setBody }) => {
 
-const Scene = () => {
+        return (
+            <group rotation={ rotation } position={ position }>
+                <Wheels />
+                <Body body={ body} setBody={ setBody} />
+            </group>
+        )
+    };
+
+    const Axel = () => {
+        extend({CylinderGeometry})
+        return (<>
+            <mesh position={[0,1,-2]} rotation={ [0,0,Math.PI/2] }>
+                
+                <cylinderGeometry attach="geometry" args={[0.05,0.05,4]} />
+                <meshLambertMaterial attach="material" color="white" roughness={0} metalness={0.1} />
+            </mesh>
+        </>)
+    }
+            
+
+    const Wheels = () => {
+        return (<>
+            <BackWheels />
+            <FrontWheels position={[0,0,1] } scale={[0.8,0.8,0.8]}/>
+        </>)
+    };
+
+    const BackWheels = ({ position }) => {
+        return (<>
+            <group position={ position }>
+                <Axel />
+                <ModelViewer
+                    model="./assets/models/wheels.gltf"                
+                />
+            </group>
+        </>)
+    };
+
+
+    const FrontWheels = ({ position, scale }) => {
+        return (<>
+            <group position={ position } scale={ scale }>
+                <Axel/>
+                <ModelViewer
+                    model="./assets/models/wheels-front.gltf"                                
+                />
+            </group>
+        </>)
+    };
 
     
-    const [body, setBodyIndex] = useState(0);
+
+    const changeBody = (currBody) => {    
+        let index  = bodies.indexOf(currBody);
+        index++;
+        if (index > bodies.length - 1) index = 0;    
+        return bodies[index];
+    }
+
+    // on click change body
+
+
+    const Body = ({position, body, setBody, rotation, scale}) => {    
+
+
+
+
+        return (<>
+            <ModelViewer
+                clickHandler = { () => setBody(changeBody(body)) }
+                model={ `/assets/models/body/${ body }.gltf` }
+                position={ position }
+                rotation={ rotation }
+                scale={ scale }
+            />
+        </>)
+    };
+    return (
+        <>
+            <Car setBody={ setBody } body={ body } position={[0,-3.5,-7]} rotation={[0,-Math.PI/9,0]}/>        
+        </>
+    )
+}
+
+const Scene = () => {
+        
     // set background color of scene    
   
     useFrame(() => {
@@ -165,18 +173,18 @@ const Scene = () => {
   
     return (
       <>
-        <pointLight position={[0, 10, 10]} intensity={1}  />
-        <pointLight position={[10, -10, 10]} intensity={1}  />
         <OrbitControls />        
-        {/* <axesHelper />
-        <gridHelper /> */}
-        <ambientLight intensity={0.3}  />        
+
+        <pointLight position={[-10, 10, -10]} radius={10} color="#3333300" intensity={1.5} castShadow />
+        <pointLight position={[10, -10, 10]} intensity={1} castShadow />        
+        <ambientLight intensity={0.3} castShadow />        
+        
         <Logo/>
 
-        <Car setBodyIndex={ setBodyIndex } body={ body } position={[0,-3.5,-7]} rotation={[0,-Math.PI/9,0]}/>
         <Base/>
         <Ground/>
 
+        <Configurator/>
       </>
     );
   };
@@ -185,11 +193,14 @@ const Scene = () => {
   
 
 createRoot(document.getElementById('root')).render(
-  <Canvas 
-    camera={{ position: [0, 0, 0], fov: 75 }}
+  <Canvas      
+    camera={{ position: [0, 0, 0], fov: 80 }}
     onCreated={({ gl }) => {
         gl.setClearColor(new THREE.Color(0x0000cc));
     }}>
+     <SoftShadows size= {{ value: 25, min: 0, max: 100 }}
+        focus= {{ value: 0, min: 0, max: 2 }}
+        samples= {{ value: 10, min: 1, max: 20, step: 1 }} />
      <Scene />
   </Canvas>,
 )
