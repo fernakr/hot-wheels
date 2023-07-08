804318ab3caae11d8ecb12a4ea7732cee2190ac7
Kristine Fernandez <hello@kristinefernandez.com>
2023-07-07 19:49:35 -0500

    xr

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import { createRoot } from 'react-dom/client'
 import React, { useRef, useState, Suspense } from 'react'
-import { Bloom, DepthOfField, EffectComposer, Noise, Vignette } from "@react-three/postprocessing";
+//import { Bloom, DepthOfField, EffectComposer, Noise, Vignette } from "@react-three/postprocessing";
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+
+import { VRButton, ARButton, XR, Controllers, Hands, Interactive } from '@react-three/xr'
+//import { Canvas } from '@react-three/fiber'
+
+
 
 import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry';
 import { PlaneGeometry } from 'three/src/geometries/PlaneGeometry.js';
@@ -17,103 +22,155 @@ import { OrbitControls, SoftShadows } from '@react-three/drei'
 import * as THREE from 'three';
 import myFont from './flash_rogers.typeface.json';
 
-let bodies = ['jordan','heart','kitten','nugget','torso'];
+let bodies = [
+    {
+        id: 'jordan',
+        name: 'Michael Jordan',
+        speed: 9,
+        agility: 8,
+        features: [
+            '6 NBA Championships',
+            '5 MVP Awards',
+        ]
+    }, 
+    {
+        id: 'heart',
+        name: 'Heart',
+        speed: 9,
+        agility: 8,
+        features: [
+            '6 NBA Championships',
+            '5 MVP Awards',
+        ]
+    }, 
+    {
+        id: 'kitten',
+        name: 'Kitten',
+        speed: 9,
+        agility: 8,
+        features: [
+            '6 NBA Championships',
+            '5 MVP Awards',
+        ]
+    },
+    {
+        id: 'nugget',
+        name: 'Chicken Nugget',
+        speed: 9,
+        agility: 8,
+        features: [
+            '6 NBA Championships',
+            '5 MVP Awards',
+        ]
+    }, 
+    {
+        id: 'torso',
+        name: 'Torso',
+        speed: 9,
+        agility: 8,
+        features: [
+            '6 NBA Championships',
+            '5 MVP Awards',
+        ]
+    }
+];
 
-const ModelViewer = ({ model, position, rotation, scale, clickHandler }) => {    
+const ModelViewer = ({ model, position, rotation, scale, clickHandler }) => {
     const gltf = useLoader(GLTFLoader, model)
     // apply position to model
 
-    return <primitive  castShadow onClick={ clickHandler } object={gltf.scene} position={position} rotation={rotation} scale={ scale } />
- }
+    return <primitive castShadow onClick={clickHandler} object={gltf.scene} position={position} rotation={rotation} scale={scale} />
+}
 
 const Logo = () => {
     let fontLoader = new FontLoader();
     let taglineFont = fontLoader.parse(myFont);
 
-    extend({TextGeometry});
+    extend({ TextGeometry });
     return (<>
-         <ModelViewer
+        <ModelViewer
             model="./assets/models/hotwheels.gltf"
             position={[0, 0, -8]}
-            rotation={[Math.PI/20,0,0]}
+            rotation={[Math.PI / 20, 0, 0]}
         />
         {/* Add text  */}
-        
-            { taglineFont && 
-                <mesh position={[-1,3,-7]}>
-                    <textGeometry attach="geometry" args={[`"We'll make a car\n out of anything!!"`, { font: taglineFont, size: 0.7, height: .01}]} />
-                    <meshStandardMaterial attach="material" color="white" />
-                </mesh>
-            }
-            
 
-    </>) 
+        {taglineFont &&
+            <mesh position={[-1, 3, -7]}>
+                <textGeometry attach="geometry" args={[`"We'll make a car\n out of anything!!"`, { font: taglineFont, size: 0.7, height: .01 }]} />
+                <meshStandardMaterial attach="material" color="white" />
+            </mesh>
+        }
+
+
+    </>)
 };
 
 
 const Ground = () => {
-    extend({PlaneGeometry})
+    extend({ PlaneGeometry })
     return (<>
-        <mesh position={[0,0,-16]}>
-            
-            <planeGeometry attach="geometry" args={[100,100]} />
+        <mesh position={[0, 0, -16]}>
+
+            <planeGeometry attach="geometry" args={[100, 100]} />
             <meshPhongMaterial attach="material" color="#000066" />
         </mesh>
     </>)
 }
 
 const Base = () => {
-    extend({CylinderGeometry})
+    extend({ CylinderGeometry })
     return (<>
-        <mesh receiveShadow position={[0,-5,-11.85]} rotation={ [0,0,0] }  >
-            
+        <mesh receiveShadow position={[0, -5, -11.85]} rotation={[0, 0, 0]}  >
+
             <shadowMaterial opacity={1} />
-            <cylinderGeometry attach="geometry" args={[6,6.25,0.4,50,2]} />
-            <meshPhysicalMaterial clearcoatRoughness={0} clearcoat={1} color="#555" roughness={0}/>
+            <cylinderGeometry attach="geometry" args={[6, 6.25, 0.4, 50, 2]} />
+            <meshPhysicalMaterial clearcoatRoughness={0} clearcoat={1} color="#555" roughness={0} />
         </mesh>
     </>)
 };
 
 const Configurator = () => {
-    
-    const [body, setBody] = useState('torso');
-        
-    
-    const Car = ({ position, rotation, body, setBody }) => {        
-        const Axel = ({ position, rotation = [Math.PI/2,0,0], size = 4 }) => {
-            extend({CylinderGeometry})
+
+    const currBody = bodies[0];
+    const [body, setBody] = useState(currBody);
+
+
+    const Car = ({ position, rotation, body, setBody }) => {
+        const Axel = ({ position, rotation = [Math.PI / 2, 0, 0], size = 4 }) => {
+            extend({ CylinderGeometry })
             return (<>
-                <group position={ position }>
-                    <mesh position={[0,1,-1]}  rotation={ rotation }>
-                        
-                        <cylinderGeometry attach="geometry" args={[0.05,0.05,size]} />
+                <group position={position}>
+                    <mesh position={[0, 1, -1]} rotation={rotation}>
+
+                        <cylinderGeometry attach="geometry" args={[0.05, 0.05, size]} />
                         <meshLambertMaterial attach="material" color="#ccc" roughness={0} metalness={0.1} />
                     </mesh>
                 </group>
             </>)
         }
-                
+
 
         const Wheels = () => {
             return (<>
                 <ModelViewer
-                    model="./assets/models/wheels.gltf"                
+                    model="./assets/models/wheels.gltf"
                 />
                 <ModelViewer
-                    position={[0,0,1] } scale={[0.8,0.8,0.8]}
-                    model="./assets/models/wheels-front.gltf"                
-                />            
+                    position={[0, 0, 1]} scale={[0.8, 0.8, 0.8]}
+                    model="./assets/models/wheels-front.gltf"
+                />
             </>)
         };
 
 
-        const Spoiler = () => {            
+        const Spoiler = () => {
             return (
                 <>
-                    <Axel position={[0,1.5,0]} rotation={ [-Math.PI/8,0,0] }  size={ 1 } />
-                    <Axel position={[1,1.5,0]} rotation={ [-Math.PI/8,0,0] }  size={ 1 } />
-                    <group position={[-0.5,3.2,-3]} >                    
-                        <mesh rotation={[Math.PI/10,0,0]}>
+                    <Axel position={[0, 1.5, 0]} rotation={[-Math.PI / 8, 0, 0]} size={1} />
+                    <Axel position={[1, 1.5, 0]} rotation={[-Math.PI / 8, 0, 0]} size={1} />
+                    <group position={[-0.5, 3.2, -3]} >
+                        <mesh rotation={[Math.PI / 10, 0, 0]}>
                             <boxGeometry args={[6, .1, 1.5]} />
                             <meshStandardMaterial color={'white'} />
                         </mesh>
@@ -124,52 +181,56 @@ const Configurator = () => {
         const Frame = () => {
             return (
                 <group>
-                    <Axel position={[0,0,3]} rotation={ [0,0,Math.PI/2] } size={ 2 }/>
-                    <Axel position={[0,0.5,0]} rotation={ [0,0,Math.PI/2] }/>
-                    <Axel position={[-0.25,0,0]} size={ 6 }/>
-                    <Axel position={[0.5,0,0]} size={ 6 }/>                    
-                </group>    
+                    <Axel position={[0, 0, 3]} rotation={[0, 0, Math.PI / 2]} size={2} />
+                    <Axel position={[0, 0.5, 0]} rotation={[0, 0, Math.PI / 2]} />
+                    <Axel position={[-0.25, 0, 0]} size={6} />
+                    <Axel position={[0.5, 0, 0]} size={6} />
+                </group>
             )
         }
         return (
-            <group rotation={ rotation } position={ position }>
-                <Spoiler/>
+            <group rotation={rotation} position={position}>
+                <Spoiler />
                 <Wheels />
                 <Frame />
-                <Body body={ body} setBody={ setBody} />
+                <Interactive                    
+                    onSelect={(event) => setBody(changeBody(body))}
+                    >
+                    <Body body={body} setBody={setBody} />
+                </Interactive>
             </group>
         )
     };
 
-    const changeBody = (currBody) => {    
-        let index  = bodies.indexOf(currBody);
+    const changeBody = (currBody) => {
+        let index = bodies.findIndex(body => body.id === currBody.id);
         index++;
-        if (index > bodies.length - 1) index = 0;    
+        if (index > bodies.length - 1) index = 0;
         return bodies[index];
     }
 
     // on click change body
 
 
-    const Body = ({position, body, setBody, rotation, scale}) => {    
+    const Body = ({ position, body, setBody, rotation, scale }) => {
 
 
 
 
         return (<>
             <ModelViewer
-                clickHandler = { () => setBody(changeBody(body)) }
-                model={ `/assets/models/body/${ body }.gltf` }
-                position={ position }
-                rotation={ rotation }
-                scale={ scale }
+                clickHandler={() => setBody(changeBody(body))}
+                model={`/assets/models/body/${body.id}.gltf`}
+                position={position}
+                rotation={rotation}
+                scale={scale}
             />
         </>)
     };
 
     return (
         <>
-            <Car setBody={ setBody } body={ body } position={[0,-3.5,-7]} rotation={[0,-Math.PI/9,0]}/>        
+            <Car setBody={setBody} body={body} position={[0, -3.5, -7]} rotation={[0, -Math.PI / 9, 0]} />
         </>
     )
 }
@@ -178,61 +239,79 @@ const ModelPreload = () => {
     return bodies.forEach((body) => {
         return (
             <ModelViewer
-                model={ `/assets/models/body/${ body }.gltf` }
+                model={`/assets/models/body/${body.id}.gltf`}
             />
         )
     });
 }
 
+const Track = () => {
+    return (
+        <ModelViewer
+            position={[-9, -4, 0]}
+            scale={[52,52,52]}
+            model="./assets/models/track.gltf"
+            />
+    )
+}
 
 const Scene = () => {
-        
+
     // set background color of scene    
-  
+
     useFrame(() => {
-      // Perform any animation or updates here
+        // Perform any animation or updates here
     });
-  
-    
-  
+
+
+
     return (
-      <>
-        <OrbitControls />        
+        <>
+            <OrbitControls />
 
-        <pointLight position={[-10, 10, -10]} radius={10} color="#ffff00" intensity={0.5} castShadow />
-        <pointLight position={[10, -10, 10]} intensity={1} castShadow />        
-        <ambientLight intensity={0.3} castShadow />        
-        
-        <Logo/>
+            <pointLight position={[-10, 10, -10]} radius={10} color="#ffff00" intensity={0.5} castShadow />
+            <pointLight position={[10, -10, 10]} intensity={1} castShadow />
+            <ambientLight intensity={0.3} castShadow />
 
-        <Base/>
-        <Ground/>
+            <Logo />
 
-        <Configurator/>
-        <ModelPreload/>
-      </>
+            
+            <Track/>
+            <Base />
+            {/* <Ground /> */}
+
+            <Configurator />
+            <ModelPreload />
+        </>
     );
-  };
+};
 
-  
-  
+
+
 
 createRoot(document.getElementById('root')).render(
-  <Canvas      
-    camera={{ position: [0, 0, 0], fov: 80 }}
-    onCreated={({ gl }) => {
-        gl.setClearColor(new THREE.Color(0x0000cc));
-    }}>        
-     <Scene />
-     
-     <EffectComposer>
-        {/* <DepthOfField focusDistance={0} focalLength={0.02} bokehScale={2} height={480} /> */}
-        {/* <Bloom luminanceThreshold={0} luminanceSmoothing={0.3} height={1000} />         */}
-        <Vignette eskil={false} offset={0.1} darkness={1.1} />
-      </EffectComposer>
+    <>
+          <VRButton />
+          <Canvas
+            camera={{ position: [0, 0, 0], fov: 80 }}
+            onCreated={({ gl }) => {
+                gl.setClearColor(new THREE.Color(0x0000cc));
+            }}>
+            <XR>
+                <Hands/>
+                <Controllers/>
+                <Scene />
 
-     <SoftShadows size= {{ value: 25, min: 0, max: 100 }}
-        focus= {{ value: 0, min: 0, max: 2 }}
-        samples= {{ value: 10, min: 1, max: 20, step: 1 }} />
-  </Canvas>,
+                {/* <EffectComposer>
+    
+                    <Vignette eskil={false} offset={0.1} darkness={1.1} />
+                </EffectComposer> */}
+
+                <SoftShadows size={{ value: 25, min: 0, max: 100 }}
+                    focus={{ value: 0, min: 0, max: 2 }}
+                    samples={{ value: 10, min: 1, max: 20, step: 1 }} />
+            </XR>
+        </Canvas>
+    </>
+,
 )
