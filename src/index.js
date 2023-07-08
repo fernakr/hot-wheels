20ced2fb073e2baa17ac67e5969d0b6cdfda1b8b
Kristine Fernandez <hello@kristinefernandez.com>
2023-07-08 10:26:20 -0500

    cfaemra position

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import { createRoot } from 'react-dom/client'
-import React, { useRef, useState, useEffect } from 'react'
+import React, { useRef, useState, useMemo, useEffect, Suspense } from 'react'
 //import { Bloom, DepthOfField, EffectComposer, Noise, Vignette } from "@react-three/postprocessing";
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+import { useSpring, a, animated, config } from '@react-spring/three'
+
 
 import { VRButton, ARButton, XR, Controllers, Hands, Interactive } from '@react-three/xr'
 //import { Canvas } from '@react-three/fiber'
@@ -17,8 +19,8 @@ import { FontLoader } from 'three/examples/jsm/loaders/FontLoader.js';
 
 //import font from 'three/examples/fonts/helvetiker_regular.typeface.json'
 
-import { Canvas, useFrame, useLoader, extend } from '@react-three/fiber'
-import { OrbitControls, SoftShadows, useAnimations, useGLTF } from '@react-three/drei'
+import { Canvas, useFrame, useLoader, extend, useThree } from '@react-three/fiber'
+import { OrbitControls, Html, useAnimations, useGLTF } from '@react-three/drei'
 import * as THREE from 'three';
 import myFont from './flash_rogers.typeface.json';
 
@@ -125,7 +127,7 @@ const Ground = () => {
     </>)
 }
 
-const Configurator = () => {
+const Configurator = ({ status, carPosition }) => {
 
     const currBody = bodies[0];
     const [body, setBody] = useState(currBody);
@@ -149,7 +151,7 @@ const Configurator = () => {
         </>)
     };
     
-    const Car = ({ position, rotation, body, setBody }) => {
+    const Car = ({ position, rotation, body, setBody, carPosition }) => {
         const Axel = ({ position, rotation = [Math.PI / 2, 0, 0], size = 4 }) => {
             extend({ CylinderGeometry })
             return (<>
@@ -202,7 +204,7 @@ const Configurator = () => {
             )
         }
         return (
-            <group rotation={rotation} position={position}>
+            <animated.group rotation={rotation} position={position}>
                 <Spoiler />
                 <Wheels />
                 <Frame />
@@ -212,7 +214,7 @@ const Configurator = () => {
                     <Body body={body} setBody={setBody} />
                 {/* </Interactive> */}
                 <Base />
-            </group>
+            </animated.group>
         )
     };
 
@@ -242,9 +244,12 @@ const Configurator = () => {
         </>)
     };
 
+    const { position } = useSpring({ position: status === 'active' ? [0, -4, -11] : carPosition,config: { duration: 1000, tension: 300, friction: 20  }, // Set the duration to 1000 milliseconds (1 second)
+    })
+
     return (
         <>
-            <Car setBody={setBody} body={body} position={[0, -4, -11]} rotation={[0, -Math.PI / 9, 0]} />
+            <Car status={ status } setBody={setBody} body={body} position={position} rotation={[0, -Math.PI / 9, 0]} />
         </>
     )
 }
@@ -265,8 +270,6 @@ const Track = () => {
 
     const { scene, animations } = useGLTF("./assets/models/track.gltf", true);
     const { actions, mixer } = useAnimations(animations, group);
-
-
     
     useEffect(() => {            
         //console.log(actions);
@@ -278,7 +281,10 @@ const Track = () => {
   }
   
 
-const Scene = () => {
+const Scene = ({ status, setStatus, carPosition }) => {
+
+    //const [ status, setStatus ] = useState('inactive');
+    //const { position } = useSpring({ position: status === 'active' ? [0, 0, -3] : [0, 0, 0],config: { duration: 1000, tension: 300, friction: 20  }});    
 
     // set background color of scene    
 
@@ -290,57 +296,202 @@ const Scene = () => {
 
     return (
         <>
-            
+            {/* <animated.group position={position}>
+                <Controls/>
+            </animated.group> */}
 
             <pointLight position={[-10, 10, -10]} radius={10} color="#ffff00" intensity={0.5} castShadow />
             <pointLight position={[10, -10, 10]} intensity={1} castShadow />
             <ambientLight intensity={0.3} castShadow />
 
             <Logo />
-            
-            <group position={[-12, -7, -7]}
-            rotation={[0,Math.PI/3,0]}
-            scale={[52,52,52]} >
-                <Track/>
-            </group>
-            
-            <ModelPreload />
-            {/* <Track/> */}
-            
-            <Ground />
+             <Suspense fallback={<Loader />}>
+                <group 
+                    position={[-12, -7, -7]}
+                    rotation={[0,Math.PI/3,0]}
+                    scale={[52,52,52]} >
+                    <Track/>
+                </group>
+                
+                <ModelPreload />
+                
+                <Ground />
+                { status != 'active' && <Html>
+                    <button className="start-button" onClick={() => setStatus('active')}>Build a Car</button>
+                </Html>
+                }
 
-            <Configurator />            
+                <Configurator status={ status } carPosition={ carPosition} />            
+            </Suspense>
         </>
     );
 };
 
+const Loader = () => {
+    return (
+        <mesh>
+            <boxGeometry args={[1, 1, 1]} />
+            <meshStandardMaterial color={'white'} />
+        </mesh>
+    )
+}
 
+
+
+// const Controls = () => {
+//     const { gl, camera } = useThree()
+//     return (
+//         /**
+//          * The args value with camera and gl are required to be passed to the OrbitControls
+//          * in order to allow the illusion of camera re-focus to work when click a pitstop.
+//          */
+//         <OrbitControls
+//         makeDefault
+//         target={[0, 0, 0]}
+//         args={[camera, gl.domElement]}
+//         maxDistance={10}
+//         minDistance={3}
+//         // vertical angle of the orbit
+//         // minPolarAngle={angleToRadians(45)}
+//         // maxPolarAngle={angleToRadians(80)}
+//         // // horizontal angle of the orbit
+//         // minAzimuthAngle={angleToRadians(-180)}
+//         // maxAzimuthAngle={angleToRadians(180)}
+//         />
+//     )
+// }
+
+const t = new THREE.Vector3();
+const defaultPosition = {
+    position: [0, 0, 0],
+    target: [0,0,-10]
+};
+
+  
+  const CameraWrapper = ({ cameraPosition, target }) => {
+    const { camera } = useThree();
+    camera.position.set(...cameraPosition);
+    camera.lookAt(t.set(...target));
+    return null;
+  };
+  
+  const ControlsWrapper = ({ target }) => {
+    const { controls } = useThree();
+    if (controls) {
+      controls.target.set(...target);
+    }
+    return null;
+  };
+  
+  function AnimateEyeToTarget({ position, target }) {
+    const { camera, controls } = useThree();
+    
+    const s = useSpring({
+      from: defaultPosition,
+      // Fun jelly-like animation
+      config: config.wobbly,
+      onStart: () => {
+        if (!controls) return;
+        controls.enabled = false;
+      },
+      onRest: () => {
+        if (!controls) return;
+        controls.enabled = true;
+      }
+    });
+  
+    s.position.start({ from: camera.position.toArray(), to: position });
+    s.target.start({
+      from: controls ? controls.target.toArray() : [0, 0, 0],
+      to: target
+    });
+  
+    const AnimateControls = useMemo(() => a(ControlsWrapper), []);
+    const AnimatedNavigation = useMemo(() => a(CameraWrapper), []);
+  
+    return (
+      <>
+        <AnimatedNavigation cameraPosition={s.position} target={s.target} />
+        <AnimateControls target={s.target} />
+      </>
+    );
+  }
+  
+  function EyeAnimation({ status, carPosition }) {
+
+
+    let targetPosition = [...carPosition]
+    targetPosition[0] = targetPosition[0] + 2;
+    targetPosition[1] = targetPosition[1] + 12;
+    targetPosition[2] = targetPosition[2] -7;
+
+    
+    const closeBy = {
+        position: [0, 0, 0],
+        target: targetPosition
+    };
+    
+    const farAway = {
+        position: [-3, 0, 10],
+        target: [0,0,-10]
+    };
+    const [cameraSettings, setCameraSettings] = useState(defaultPosition);
+  
+    useEffect(() => {
+      if (status === 'active') {
+        setCameraSettings(closeBy);
+      } else {
+        setCameraSettings(farAway);
+      }
+    }, [status]);
+  
+    return (
+      <>
+        <AnimateEyeToTarget
+            
+          position={cameraSettings.position}
+          target={cameraSettings.target}
+        />
+      </>
+    );
+  }
+
+
+const App = () => {
+    const [ status, setStatus ] = useState('inactive');
+    const carPosition = [0, -12, -11];
+    return (
+        <Canvas
+        //camera={{ position: [0, 0,0], fov: 80 }}
+        onCreated={({ gl }) => {
+            gl.setClearColor(new THREE.Color(0x0000cc));
+        }}>
+        <OrbitControls makeDefault />
+        <EyeAnimation status={ status } carPosition={ carPosition }/>
+
+            
+        {/* <XR> */}
+            {/* <Hands/>
+            <Controllers/> */}
+            <Scene status={ status } setStatus={ setStatus }  carPosition={ carPosition }/>
+
+            {/* <EffectComposer>
+
+                <Vignette eskil={false} offset={0.1} darkness={1.1} />
+            </EffectComposer> */}
+
+            {/* <SoftShadows size={{ value: 25, min: 0, max: 100 }}
+                focus={{ value: 0, min: 0, max: 2 }}
+                samples={{ value: 10, min: 1, max: 20, step: 1 }} /> */}
+        {/* </XR> */}
+    </Canvas>
+    )
+}
 
 
 createRoot(document.getElementById('root')).render(
     <>
-          {/* <VRButton /> */}
-          <Canvas
-            //camera={{ position: [0, 0,0], fov: 80 }}
-            onCreated={({ gl }) => {
-                gl.setClearColor(new THREE.Color(0x0000cc));
-            }}>
-                <OrbitControls />
-            {/* <XR> */}
-                {/* <Hands/>
-                <Controllers/> */}
-                <Scene />
-
-                {/* <EffectComposer>
-    
-                    <Vignette eskil={false} offset={0.1} darkness={1.1} />
-                </EffectComposer> */}
-
-                <SoftShadows size={{ value: 25, min: 0, max: 100 }}
-                    focus={{ value: 0, min: 0, max: 2 }}
-                    samples={{ value: 10, min: 1, max: 20, step: 1 }} />
-            {/* </XR> */}
-        </Canvas>
+         <App/>
     </>
 ,
 )
